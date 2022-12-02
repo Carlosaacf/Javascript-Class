16ae7d4eda2b022c32b2903515a8c450a3d143e1
Carlos-Fonseca <carlos.fonseca@ibm.com>
2022-12-02 09:06:55 -0300

    Operadores logicos for in for of

---
+++ aula14.js
@@ -0,0 +1,19 @@
+let controle=7
+
+switch(controle){
+    case 1:
+    console.log("ola")
+    break
+
+    case 2:
+        console.log("good morning")
+    break
+
+    case 3:
+        console.log("bon jour")
+        break
+
+        default:
+            console.log("sem opções validas")
+            break
+}
---
+++ aula15.js
@@ -0,0 +1,8 @@
+console.log("inicio do programa")
+
+for (let i = 0; i < 10; i++) {
+    console.log("CFB Cursos")
+    
+}
+
+console.log("FIM")
