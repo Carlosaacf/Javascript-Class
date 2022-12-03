ca31e0210e0550eff6dbf65077f022085c93f40b
Carlos-Fonseca <carlos.fonseca@ibm.com>
2022-12-03 08:02:11 -0300

    modulos de funcao do javascript

---
+++ aula21.js
@@ -0,0 +1,15 @@
+function canal() {
+    let n1=13
+    let n2=3
+    let res=n1*n2
+    if (res%2==0){
+        return "PAR"
+    } else{
+        return "IMPAR"
+    }
+}
+
+let num=canal()
+
+console.log(num)
+
---
+++ aula22.js
@@ -0,0 +1,21 @@
+function soma(p1,p2){
+    console.log(p1+p2)
+}
+soma(22,45)
+
+function soma2(n1=0, n2=0){
+    return n1+n2
+}
+
+console.log(soma2(14,22))
+
+let valor=0
+
+console.log(valor)
+function add(v){
+    return valor+v
+}
+
+valor=add(25)
+valor=add(3)
+console.log(valor)
